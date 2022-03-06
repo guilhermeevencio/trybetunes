7a944cc960f50f1db2150350b2f860dbc16426d6
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-06 19:28:46 -0300

    Dinaliza a função que exibe o nome de usuário inserido e a validação de login, cumprindo o que foi pedido no requisito 3.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class extends React.Component {
-  handleUser = () => {
-    getUser();
-  };
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+    };
+  }
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({ userName: user.name });
+  }
 
   render() {
+    const { userName } = this.state;
     return (
       <header data-testid="header-component">
-        <h3 data-testid="header-user-name">{this.handleUser()}</h3>
-        <Link to="/">Login</Link>
+        <h3 data-testid="header-user-name">
+          { userName || <Loading />}
+        </h3>
       </header>
     );
   }
