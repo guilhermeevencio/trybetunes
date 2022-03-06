52ad169115d61c58c47ae351f6624a6af8680c91
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-06 09:46:34 -0300

    Inicia a construção da página de login, fazendo as validaçoes necessarias de acordo com o requisito 2 e também cria o componente loading.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChanges = this.handleChanges.bind(this);
+    this.state = {
+      isDisabled: true,
+    };
+  }
+
+  handleChanges(event) {
+    event.preventDefault();
+    const minNameLength = 3;
+    this.setState(() => {
+      const { value } = event.target;
+      let disabled = true;
+      if (value.length >= minNameLength) {
+        disabled = false;
+      }
+      return {
+        inputName: value,
+        isDisabled: disabled,
+      };
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { inputName } = this.state;
+    createUser({ name: inputName });
+  }
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-login">
-        Login
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            onChange={ this.handleChanges }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            onClick={ this.handleSubmit }
+            disabled={ isDisabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
