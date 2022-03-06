96316f8fa00419688e6802180e62d253a49df438
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-05 22:25:52 -0300

    Cria todas as rotas para podermos acessar as páginas criadas, comforme pedido no requisito 01, que foi concluido.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Album from './pages/Abum';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<h1>TrybeTunes - O inicio</h1>);
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile" exact component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
