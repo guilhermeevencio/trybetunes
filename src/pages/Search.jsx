8e2e45a2be3ae2a56f7cbe8b47fd868b1bb7668d
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-06 20:17:52 -0300

    Cria o formulario responsável por receber os dados no componente de pesquisa, cumprindo o que foi pedido no requisito 5.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,54 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+    };
+  }
+
+  handleChanges = (event) => {
+    event.preventDefault();
+    const minNameLength = 2;
+    this.setState(() => {
+      const { value } = event.target;
+      let disabled = true;
+      if (value.length >= minNameLength) {
+        disabled = false;
+      }
+      return {
+        inputName: value,
+        isDisabled: disabled,
+        loaded: false,
+      };
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleChanges }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            onClick={ this.handleSubmit }
+            disabled={ isDisabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
